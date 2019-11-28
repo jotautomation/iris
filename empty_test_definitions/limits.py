dacf3736a74ae955d0635e435b19b2ec6347457f
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2019-11-28 14:20:24 +0200

    Use word measurement instead of result
    Because result is used to define pass/fail results later
    
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- empty_test_definitions/limits.py
+++ empty_test_definitions/limits.py
@@ -1,22 +1,22 @@
 LIMITS = {}
 
 LIMITS['second'] = {
-    'Measurement1': lambda result: 90 < result < 100,
-    'Measurement2': lambda result: result == [123, 456, 1, 3],
-    'Measurement3': lambda result: 90 <= result <= 100,
-    'Measurement4': lambda result: result.contains('123abc'),
-    'Measurement5': lambda result: complicated_limit_definition(result)
+    'Measurement1': lambda measurement: 90 < measurement < 100,
+    'Measurement2': lambda measurement: measurement == [123, 456, 1, 3],
+    'Measurement3': lambda measurement: 90 <= measurement <= 100,
+    'Measurement4': lambda measurement: measurement.contains('123abc'),
+    'Measurement5': lambda measurement: complicated_limit_definition(measurement)
     }
 
 LIMITS['first'] = {
-    'Voltage': lambda result: 90 < result < 100,
+    'Voltage': lambda measurement: 90 < measurement < 100,
     }
 
 LIMITS['third'] = {
-    'Voltage': lambda result: 90 < result < 100,
+    'Voltage': lambda measurement: 90 < measurement < 100,
     }
 
 
-def complicated_limit_definition(result):
+def complicated_limit_definition(measurement):
     # Must return true/false
     pass
