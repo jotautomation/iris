ee8816f7e0c890cbbbdb73a7c176d6811468a79b
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2020-02-06 10:19:06 +0200

    Run with multiple duts
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- empty_test_definitions/test_report_writer.py
+++ empty_test_definitions/test_report_writer.py
@@ -7,16 +7,18 @@ import pathlib
 import json2html
 
 
-def create_report(json_report, dut):
+def create_report(json_report, duts):
     """Creates and stores report for DUT(s)"""
 
     current = datetime.datetime.now()
+
+    filename = '_'.join(['%s-%s' % (key, value) for (key, value) in duts.items()])
 
     report_path = (
         pathlib.Path.cwd() / 'results' / str(current.year) / str(current.month) / str(current.day)
     )
     report_path.mkdir(parents=True, exist_ok=True)
 
-    report_file_path = report_path / (dut + '.html')
+    report_file_path = report_path / (filename + '.html')
 
     report_file_path.write_text(json2html.json2html.convert(json=json_report))
