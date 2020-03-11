4c6d12e66ba302dd154acc694f63f7b15e11334e
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2020-03-11 13:30:49 +0200

    Fix setup.py
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="super_simple_test_sequencer",
-    version="0.1.1",
+    version="0.2.0",
     license="MIT License",
     author="JOT Automation Ltd.",
     author_email="rami.rahikkala@jotautomation.com",
@@ -21,10 +21,10 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://www.jotautomation.com",
-    packages=["empty_test_definitions/", "test_runner/"],
+    packages=["test_definition_template/common", "test_definition_template/example_sequence", "test_runner/", "listener"],
     scripts=["super_simple_test_runner.py"],
     #    py_modules=['super_simple_test_runner', 'test_case', 'test_report_writer' 'test_report_writer'],
-    install_requires=["wheel", "json2html"],
+    install_requires=["wheel", "json2html", "tornado"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
