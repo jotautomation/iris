b0423812bc447afd6b0736aac39f09cf9b810752
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2020-12-11 10:44:27 +0200

    Show all failed steps if more than one
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- test_runner/runner.py
+++ test_runner/runner.py
@@ -217,8 +217,12 @@ def run_test_runner(test_control, message_queue, progess_queue, dut_sn_queue):
 
                     if not all([r[1]["result"] for r in results[dut_sn][test_case].items()]):
                         overall_result = False
-                        dut_status[dut_name]['test_status'] = 'fail'
-                        dut_status[dut_name]['failed_step'] = test_case
+                        if 'failed_step' in dut_status[dut_name]:
+                            dut_status[dut_name]['failed_step'] = (
+                                dut_status[dut_name]['failed_step'] + ', ' + test_case
+                            )
+                        else:
+                            dut_status[dut_name]['failed_step'] = test_case
                         if hasattr(test_instance, 'clean_fail'):
                             test_instance.clean_fail(common_definitions.INSTRUMENTS, dut_sn)
 
