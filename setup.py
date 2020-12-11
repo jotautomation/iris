669f92eb9194701e06d6004e327efce5f67836ff
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2020-12-11 14:03:35 +0200

    Bump version
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ get_ui()
 
 setup(
     name="super_simple_test_sequencer",
-    version="0.12.3",
+    version="0.12.4",
     license="MIT License",
     author="JOT Automation Ltd.",
     author_email="rami.rahikkala@jotautomation.com",
