25b6ec1c46251e650d01b7add2dc45c3715328a2
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2020-12-11 15:00:22 +0200

    Fix failed step reporting
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- test_runner/runner.py
+++ test_runner/runner.py
@@ -159,6 +159,8 @@ def run_test_runner(test_control, message_queue, progess_queue, dut_sn_queue):
 
             tests = [t for t in test_definitions.TESTS if t not in test_definitions.SKIP]
 
+            failed_steps = {}
+
             for dut_key, dut_value in duts.items():
                 dut_status[dut_key] = {
                     'step': None,
@@ -166,8 +168,11 @@ def run_test_runner(test_control, message_queue, progess_queue, dut_sn_queue):
                     'test_status': None,
                     'sn': dut_value['sn'],
                 }
+                failed_steps[dut_key] = {}
+
 
             results["start_time"] = datetime.datetime.now()
+
 
             for test_case in tests:
 
@@ -210,12 +215,12 @@ def run_test_runner(test_control, message_queue, progess_queue, dut_sn_queue):
 
                     if not all([r[1]["result"] for r in results[dut_sn][test_case].items()]):
                         overall_result = False
-                        if 'failed_step' in dut_status[dut_name]:
-                            dut_status[dut_name]['failed_step'] = (
-                                dut_status[dut_name]['failed_step'] + ', ' + test_case
+                        if 'failed_step' in failed_steps[dut_name]:
+                            failed_steps[dut_name]['failed_step'] = (
+                                failed_steps[dut_name]['failed_step'] + ', ' + test_case
                             )
                         else:
-                            dut_status[dut_name]['failed_step'] = test_case
+                            failed_steps[dut_name]['failed_step'] = test_case
                         if hasattr(test_instance, 'clean_fail'):
                             test_instance.clean_fail(common_definitions.INSTRUMENTS, dut_sn)
 
@@ -238,8 +243,10 @@ def run_test_runner(test_control, message_queue, progess_queue, dut_sn_queue):
                     report_progress('testing', dut_status, sequence=sequence)
 
             for dut_name, dut_value in duts.items():
-                if 'failed_step' in dut_status[dut_name]:
+                if 'failed_step' in failed_steps[dut_name]:
                     dut_status[dut_name]['test_status'] = 'fail'
+                    dut_status[dut_name]['failed_step'] = failed_steps[dut_name]['failed_step']
+
                 else:
                     dut_status[dut_name]['test_status'] = 'pass'
 
