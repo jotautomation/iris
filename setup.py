73c142e90d44ce5fa43800ec20478d41092b5e1f
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2021-04-21 13:46:59 +0300

    Bump version
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ get_ui()
 
 setup(
     name="jot-iris",
-    version="1.1.0",
+    version="1.2.0",
     license="MIT License",
     author="JOT Automation Ltd.",
     author_email="rami.rahikkala@jotautomation.com",
