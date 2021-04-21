cc9c84241bd4deecc5ddfff52bc84478e71d7b41
Rami Rahikkala <rami.rahikkala@jotautomation.com>
2021-04-21 13:43:31 +0300

    Add missing FlowControl definition
    Signed-off-by: Rami Rahikkala <rami.rahikkala@jotautomation.com>

--- test_definition_template/common/common_definitions.py
+++ test_definition_template/common/common_definitions.py
@@ -2,9 +2,19 @@
 import os
 from test_runner.test_position import TestPosition
 from test_runner.dut import Dut
+from test_runner.test_case import FlowControl
 
 
-TEST_POSITIONS = [TestPosition('1', 'left'), TestPosition('2', 'middle'), TestPosition('3', 'right')]
+TEST_POSITIONS = [
+    TestPosition('1', 'left'),
+    TestPosition('2', 'middle'),
+    TestPosition('3', 'right'),
+]
+
+# FlowControl.CONTINUE keeps testing even some test fails
+# FlowControl.STOP_ON_FAIL stops on first fail
+
+FLOW_CONTROL = FlowControl.CONTINUE
 
 # Wait DUT serial number from UI
 SN_FROM_UI = True
