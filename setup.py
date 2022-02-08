ef265f04e8c1b509d28f2cc5f23d156fea3dda54
Atte Rankinen <atte.rankinen@jotautomation.com>
2022-02-08 04:32:44 -0800

    Remove skipped positions from test loop

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ get_ui()
 
 setup(
     name="jot-iris",
-    version="1.19.15",
+    version="1.19.16",
     license="MIT License",
     author="JOT Automation Ltd.",
     author_email="info@jotautomation.com",
--- test_runner/runner.py
+++ test_runner/runner.py
@@ -656,6 +656,7 @@ def run_test_runner(test_control, message_queue, progess_queue, dut_sn_queue, li
                             test_position_instance.stop_looping
                             or test_position_instance.dut is None
                         ):
+                            test_position_instance.stop_looping = True
                             logger.info(
                                 "Skip position %s from test loop",
                                 test_position_instance.name
@@ -707,6 +708,7 @@ def run_test_runner(test_control, message_queue, progess_queue, dut_sn_queue, li
                             test_position_instance.stop_looping
                             or test_position_instance.dut is None
                         ):
+                            test_position_instance.stop_looping = True
                             logger.info(
                                 "Skip position %s from test loop",
                                 test_position_instance.name
